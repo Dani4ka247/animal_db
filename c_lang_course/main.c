5653980b8d9f87bcc03dac06e8712d6d7ffb5d86
Dani4ka <dani4ka132@gmail.com>
2026-02-21 18:29:49 +0300

    Initial Commit

---
+++ c_lang_course/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c_lang_course
+//
+//  Created by Dani4ka on 21.02.2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
